384554086cd5fdd7b92caae9db44236edcb2a1ff
Ricardo Loureiro <ricardo_loureiro@ecgs.com>
2019-06-28 09:15:28 -0300

    Inicial

---
+++ Application/src/application/Core.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import interfaces.ICore;
+import interfaces.IPluginController;
+import interfaces.IUIController;
+
+/**
+ *
+ * @author aluno
+ */
+public class Core implements ICore {
+
+    public Core() {
+        uiController = new UIController();
+        pluginController = new PluginController();
+        uiController.initialize();
+        pluginController.initialize(this);
+    }
+    @Override
+    public IUIController getUIController() {
+        return uiController;
+    }
+
+    @Override
+    public IPluginController getPluginController() {
+        return pluginController;
+    }
+    
+    private IUIController uiController;
+    private IPluginController pluginController;
+}
